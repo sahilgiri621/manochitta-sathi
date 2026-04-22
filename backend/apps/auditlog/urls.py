9568d80cdeec13d20ede038ea8bbed4bec160664
Sahil Giri <sahilgiri621@gmail.com>
2026-04-22 10:28:49 +0545

    remove unused audit log module

--- backend/apps/auditlog/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import AdminActionViewSet
-
-router = DefaultRouter()
-router.register("", AdminActionViewSet, basename="admin-actions")
-
-urlpatterns = router.urls
